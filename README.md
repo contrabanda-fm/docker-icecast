0b82b6990821ca6e7d62b300d811e3a5893525c7
jlegido <javi@legido.com>
2024-03-16 13:28:16 +0100

    Added env variables to create mounts

--- README.md
+++ README.md
@@ -15,15 +15,64 @@ Docker image for [Icecast 2](https://icecast.org/)
 
 ```
 docker run -d -p 8000:8000 ghcr.io/contrabanda-fm/docker-icecast
+
+docker run \
+    --name icecast \
+    -e ICECAST_SOURCE_PASSWORD=sourcesecret \
+    -e ICECAST_ADMIN_PASSWORD=adminsecret \
+    -e ICECAST_RELAY_PASSWORD=relaysecret \
+    -e ICECAST_ADMIN_USERNAME=myuser \
+    -e ICECAST_MOUNT_1_MOUNT_NAME="/contrabanda" \
+    -e ICECAST_MOUNT_1_PASSWORD="mountsecret" \
+    -e ICECAST_MOUNT_1_MAX_LISTENERS="900" \
+    -e ICECAST_MOUNT_1_MAX_LISTENER_DURATION="36000" \
+    -e ICECAST_MOUNT_1_STREAM_NAME="Contrabanda FM" \
+    -e ICECAST_MOUNT_1_STREAM_DESCRIPTION="Radio Lliure i Autogestionada de Barcelona" \
+    -e ICECAST_MOUNT_1_STREAM_URL="https://www.contrabanda.org" \
+    -e ICECAST_MOUNT_1_GENRE="RadioLliure" \
+    -e ICECAST_MOUNT_1_BITRATE="96" \
+    -e ICECAST_MOUNT_2_MOUNT_NAME="/carrer" \
+    -e ICECAST_MOUNT_2_PASSWORD="mount2secret" \
+    -e ICECAST_MOUNT_2_MAX_LISTENERS="100" \
+    -e ICECAST_MOUNT_2_MAX_LISTENER_DURATION="36000" \
+    -e ICECAST_MOUNT_2_STREAM_NAME="Contrabanda FM" \
+    -e ICECAST_MOUNT_2_STREAM_DESCRIPTION="Canal per emissions al carrer de Contrabanda FM" \
+    -e ICECAST_MOUNT_2_STREAM_URL="https://www.contrabanda.org" \
+    -e ICECAST_MOUNT_2_GENRE="RadioLliure" \
+    -e ICECAST_MOUNT_2_BITRATE="96" \
+    -p 8000:8000 \
+    -d \
+    ghcr.io/contrabanda-fm/docker-icecast
 ```
 
 Supported ENV variables:
 
+* For general settings:
+
 ```
-ICECAST_SOURCE_PASSWORD, ICECAST_ADMIN_PASSWORD, ICECAST_RELAY_PASSWORD
-ICECAST_ADMIN_USERNAME, ICECAST_ADMIN_EMAIL
-ICECAST_LOCATION, ICECAST_HOSTNAME
-ICECAST_MAX_CLIENTS, ICECAST_MAX_SOURCES
+ICECAST_ADMIN_EMAIL
+ICECAST_ADMIN_PASSWORD
+ICECAST_ADMIN_USERNAME
+ICECAST_HOSTNAME
+ICECAST_LOCATION
+ICECAST_MAX_CLIENTS
+ICECAST_MAX_SOURCES
+ICECAST_RELAY_PASSWORD
+ICECAST_SOURCE_PASSWORD
+```
+
+* For each one of mount point start variable with "ICECAST_MOUNT_X" where "X" its an integer from 1 onwards:
+
+```
+ICECAST_MOUNT_1_MOUNT_NAME
+ICECAST_MOUNT_1_PASSWORD
+ICECAST_MOUNT_1_MAX_LISTENERS
+ICECAST_MOUNT_1_MAX_LISTENER_DURATION
+ICECAST_MOUNT_1_STREAM_NAME
+ICECAST_MOUNT_1_STREAM_DESCRIPTION
+ICECAST_MOUNT_1_STREAM_URL
+ICECAST_MOUNT_1_GENRE
+ICECAST_MOUNT_1_BITRATE
 ```
 
 # Credits
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,31 +1,93 @@
 #!/bin/sh
 
-if [ -n "$ICECAST_SOURCE_PASSWORD" ]; then
-    sed -i "s/<source-password>[^<]*<\/source-password>/<source-password>$ICECAST_SOURCE_PASSWORD<\/source-password>/g" /etc/icecast.xml
-fi
-if [ -n "$ICECAST_RELAY_PASSWORD" ]; then
-    sed -i "s/<relay-password>[^<]*<\/relay-password>/<relay-password>$ICECAST_RELAY_PASSWORD<\/relay-password>/g" /etc/icecast.xml
-fi
-if [ -n "$ICECAST_ADMIN_PASSWORD" ]; then
-    sed -i "s/<admin-password>[^<]*<\/admin-password>/<admin-password>$ICECAST_ADMIN_PASSWORD<\/admin-password>/g" /etc/icecast.xml
-fi
-if [ -n "$ICECAST_ADMIN_USERNAME" ]; then
-    sed -i "s/<admin-user>[^<]*<\/admin-user>/<admin-user>$ICECAST_ADMIN_USERNAME<\/admin-user>/g" /etc/icecast.xml
-fi
-if [ -n "$ICECAST_ADMIN_EMAIL" ]; then
-    sed -i "s/<admin>[^<]*<\/admin>/<admin>$ICECAST_ADMIN_EMAIL<\/admin>/g" /etc/icecast.xml
-fi
-if [ -n "$ICECAST_LOCATION" ]; then
-    sed -i "s/<location>[^<]*<\/location>/<location>$ICECAST_LOCATION<\/location>/g" /etc/icecast.xml
-fi
-if [ -n "$ICECAST_HOSTNAME" ]; then
-    sed -i "s/<hostname>[^<]*<\/hostname>/<hostname>$ICECAST_HOSTNAME<\/hostname>/g" /etc/icecast.xml
-fi
-if [ -n "$ICECAST_MAX_CLIENTS" ]; then
-    sed -i "s/<clients>[^<]*<\/clients>/<clients>$ICECAST_MAX_CLIENTS<\/clients>/g" /etc/icecast.xml
-fi
-if [ -n "$ICECAST_MAX_SOURCES" ]; then
-    sed -i "s/<sources>[^<]*<\/sources>/<sources>$ICECAST_MAX_SOURCES<\/sources>/g" /etc/icecast.xml
-fi
+# Path to the Icecast configuration file
+path_icecast_configuration_file="/etc/icecast.xml"
 
+# Marker file to indicate whether the block has been added
+marker_file="mount_block_added"
+
+previous_block_number="0"
+
+xml_block=""
+
+env_sorted=$(env | sort)
+
+# Function to add mount blocks to XML file before </icecast> tag
+add_mount_blocks() {
+    # Loop through environment variables and add mount blocks
+    for var in $(echo "$env_sorted" | grep '^ICECAST_MOUNT_' | cut -d= -f1); do
+        block_number=$(echo "$var" | cut -d'_' -f3)
+        # Extract substring after third underscore
+        trimmed_var=$(echo "$var" | cut -d"_" -f4- | tr '[:upper:]' '[:lower:]' | tr '_' '-')
+        xml_element=$(build_xml_element "$var")
+        # Check if block_number changes and previous value is not 0
+        if [ "$block_number" != "$previous_block_number" ]; then
+            if [ "$previous_block_number" != "0" ]; then
+                xml_block="$xml_block\t</mount>\n"  # Close the previous mount block
+            fi
+            xml_block="$xml_block\t<mount type=\"normal\">\n"  # Open a new mount block
+        fi
+        xml_block="$xml_block\t\t$xml_element\n"  # Append xml_element to xml_block with two tabs
+        previous_block_number="$block_number"  # Update previous_block_number
+    done
+
+    # Append closing tag for the last mount block
+    if [ "$previous_block_number" != "0" ]; then
+        xml_block="$xml_block\t</mount>\n"
+    fi
+
+    # Check if the marker file exists
+    if [ ! -f "$marker_file" ]; then
+        # Add the mount block before the </icecast> tag
+        awk -v block="$xml_block" '/<\/icecast>/ {print block} 1' "$path_icecast_configuration_file" > "$path_icecast_configuration_file.tmp" && mv "$path_icecast_configuration_file.tmp" "$path_icecast_configuration_file"
+        # Create the marker file
+        touch "$marker_file"
+    fi
+}
+
+# Function to build XML element from environment variable name
+build_xml_element() {
+    local var_name="$1"
+    local trimmed_var=$(echo "$var_name" | cut -d'_' -f4- | tr '[:upper:]' '[:lower:]' | tr '_' '-')
+    local value=$(eval echo "\$$var_name")  # Perform variable expansion
+    echo "<${trimmed_var}>${value}</${trimmed_var}>"
+}
+
+general_settings() {
+    if [ -n "$ICECAST_SOURCE_PASSWORD" ]; then
+        sed -i "s/<source-password>[^<]*<\/source-password>/<source-password>$ICECAST_SOURCE_PASSWORD<\/source-password>/g" /etc/icecast.xml
+    fi
+    if [ -n "$ICECAST_RELAY_PASSWORD" ]; then
+        sed -i "s/<relay-password>[^<]*<\/relay-password>/<relay-password>$ICECAST_RELAY_PASSWORD<\/relay-password>/g" /etc/icecast.xml
+    fi
+    if [ -n "$ICECAST_ADMIN_PASSWORD" ]; then
+        sed -i "s/<admin-password>[^<]*<\/admin-password>/<admin-password>$ICECAST_ADMIN_PASSWORD<\/admin-password>/g" /etc/icecast.xml
+    fi
+    if [ -n "$ICECAST_ADMIN_USERNAME" ]; then
+        sed -i "s/<admin-user>[^<]*<\/admin-user>/<admin-user>$ICECAST_ADMIN_USERNAME<\/admin-user>/g" /etc/icecast.xml
+    fi
+    if [ -n "$ICECAST_ADMIN_EMAIL" ]; then
+        sed -i "s/<admin>[^<]*<\/admin>/<admin>$ICECAST_ADMIN_EMAIL<\/admin>/g" /etc/icecast.xml
+    fi
+    if [ -n "$ICECAST_LOCATION" ]; then
+        sed -i "s/<location>[^<]*<\/location>/<location>$ICECAST_LOCATION<\/location>/g" /etc/icecast.xml
+    fi
+    if [ -n "$ICECAST_HOSTNAME" ]; then
+        sed -i "s/<hostname>[^<]*<\/hostname>/<hostname>$ICECAST_HOSTNAME<\/hostname>/g" /etc/icecast.xml
+    fi
+    if [ -n "$ICECAST_MAX_CLIENTS" ]; then
+        sed -i "s/<clients>[^<]*<\/clients>/<clients>$ICECAST_MAX_CLIENTS<\/clients>/g" /etc/icecast.xml
+    fi
+    if [ -n "$ICECAST_MAX_SOURCES" ]; then
+        sed -i "s/<sources>[^<]*<\/sources>/<sources>$ICECAST_MAX_SOURCES<\/sources>/g" /etc/icecast.xml
+    fi
+}
+
+# Legacy replacement
+general_settings
+
+# Add mount blocks to XML file
+add_mount_blocks
+
+# Execute the CMD or whatever command is provided
 exec "$@"
